f74ac52d1e2519256b11657db6241560a8356680
pgaetani <pier.gaetani@gmail.com>
2025-01-23 15:04:03 +0100

    template files moved, scripts adjusted, tiltfile adjustments, launch settings

--- generate-env-files.sh
+++ generate-env-files.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-if [ -n "$NOTIFY_CREDENTIALS" ]; then
-  export PASSWORD_STORE_DIR="$NOTIFY_CREDENTIALS"
-elif [ -z "$PASSWORD_STORE_DIR" ]; then
-  echo -n "Enter the full path to your local checkout of \`notifications-credentials\`: "
-  read PASSWORD_STORE_DIR
-  export PASSWORD_STORE_DIR
-fi
+# if [ -n "$NOTIFY_CREDENTIALS" ]; then
+#   export PASSWORD_STORE_DIR="$NOTIFY_CREDENTIALS"
+# elif [ -z "$PASSWORD_STORE_DIR" ]; then
+#   echo -n "Enter the full path to your local checkout of \`notifications-credentials\`: "
+#   read PASSWORD_STORE_DIR
+#   export PASSWORD_STORE_DIR
+# fi
 
 echo -n "Enter your local development AWS SQS queue prefix (eg \`local_dev_sam_\`): "
 read TMPL_NOTIFICATIONS_QUEUE_PREFIX
 export TMPL_NOTIFICATIONS_QUEUE_PREFIX="${TMPL_NOTIFICATIONS_QUEUE_PREFIX}"
+export TMPL_NOTIFICATIONS_QUEUE_PREFIX="local_dev_$(whoami)_"
 
 echo -n "Enter your local development user's aws_access_key_id: "
 read TMPL_AWS_ACCESS_KEY_ID
@@ -20,18 +21,18 @@ echo -n "Enter your local development user's aws_secret_access_key: "
 read TMPL_AWS_SECRET_ACCESS_KEY
 export TMPL_AWS_SECRET_ACCESS_KEY="${TMPL_AWS_SECRET_ACCESS_KEY}"
 
-echo -n "Reading secrets from \`notifications-credentials\` ... "
-export TMPL_MMG_API_KEY=$(pass credentials/mmg | tail -n 6 | grep "API key" | cut -d" " -f3)
-export TMPL_FIRETEXT_API_KEY=$(pass credentials/firetext | tail -n 6 | grep "API key" | cut -d" " -f3)
-export TMPL_ZENDESK_API_KEY=$(pass credentials/preview/paas/environment-variables|grep ZENDESK_API_KEY|cut -d" " -f2|tr -d '"')
+# echo -n "Reading secrets from \`notifications-credentials\` ... "
+# export TMPL_MMG_API_KEY=$(pass credentials/mmg | tail -n 6 | grep "API key" | cut -d" " -f3)
+# export TMPL_FIRETEXT_API_KEY=$(pass credentials/firetext | tail -n 6 | grep "API key" | cut -d" " -f3)
+# export TMPL_ZENDESK_API_KEY=$(pass credentials/preview/paas/environment-variables|grep ZENDESK_API_KEY|cut -d" " -f2|tr -d '"')
 
-export TMPL_NOTIFY_API_SENTRY_DSN=$(pass credentials/preview/paas/environment-variables|grep NOTIFICATIONS_API_SENTRY_DSN|cut -d" " -f2|tr -d '"')
-export TMPL_NOTIFY_ADMIN_SENTRY_DSN=$(pass credentials/preview/paas/environment-variables|grep NOTIFICATIONS_ADMIN_SENTRY_DSN|cut -d" " -f2|tr -d '"')
-export TMPL_DOCUMENT_DOWNLOAD_API_SENTRY_DSN=$(pass credentials/preview/paas/environment-variables|grep DOCUMENT_DOWNLOAD_API_SENTRY_DSN|cut -d" " -f2|tr -d '"')
-export TMPL_DOCUMENT_DOWNLOAD_FRONTEND_SENTRY_DSN=$(pass credentials/preview/paas/environment-variables|grep DOCUMENT_DOWNLOAD_FRONTEND_SENTRY_DSN|cut -d" " -f2|tr -d '"')
-export TMPL_TEMPLATE_PREVIEW_API_SENTRY_DSN=$(pass credentials/preview/paas/environment-variables|grep TEMPLATE_PREVIEW_SENTRY_DSN|cut -d" " -f2|tr -d '"')
-export TMPL_ANTIVIRUS_API_SENTRY_DSN=$(pass credentials/preview/paas/environment-variables|grep NOTIFICATIONS_ANTIVIRUS_SENTRY_DSN|cut -d" " -f2|tr -d '"')
-export TMPL_APPLICATIONINSIGHTS_CONNECTION_STRING=$(pass credentials/preview/paas/environment-variables|grep APPLICATIONINSIGHTS_CONNECTION_STRING|cut -d" " -f2|tr -d '"')
+# export TMPL_NOTIFY_API_SENTRY_DSN=$(pass credentials/preview/paas/environment-variables|grep NOTIFICATIONS_API_SENTRY_DSN|cut -d" " -f2|tr -d '"')
+# export TMPL_NOTIFY_ADMIN_SENTRY_DSN=$(pass credentials/preview/paas/environment-variables|grep NOTIFICATIONS_ADMIN_SENTRY_DSN|cut -d" " -f2|tr -d '"')
+# export TMPL_DOCUMENT_DOWNLOAD_API_SENTRY_DSN=$(pass credentials/preview/paas/environment-variables|grep DOCUMENT_DOWNLOAD_API_SENTRY_DSN|cut -d" " -f2|tr -d '"')
+# export TMPL_DOCUMENT_DOWNLOAD_FRONTEND_SENTRY_DSN=$(pass credentials/preview/paas/environment-variables|grep DOCUMENT_DOWNLOAD_FRONTEND_SENTRY_DSN|cut -d" " -f2|tr -d '"')
+# export TMPL_TEMPLATE_PREVIEW_API_SENTRY_DSN=$(pass credentials/preview/paas/environment-variables|grep TEMPLATE_PREVIEW_SENTRY_DSN|cut -d" " -f2|tr -d '"')
+# export TMPL_ANTIVIRUS_API_SENTRY_DSN=$(pass credentials/preview/paas/environment-variables|grep NOTIFICATIONS_ANTIVIRUS_SENTRY_DSN|cut -d" " -f2|tr -d '"')
+# export TMPL_APPLICATIONINSIGHTS_CONNECTION_STRING=$(pass credentials/preview/paas/environment-variables|grep APPLICATIONINSIGHTS_CONNECTION_STRING|cut -d" " -f2|tr -d '"')
 
 echo -e "Done.\n"
 
@@ -50,6 +51,6 @@ for service in \
   "template-preview-api"
 do
   echo -n "Generating private/${service}.env ... "
-  cat ${service}.env.tmpl | envsubst > private/${service}.env
+  cat templates/${service}.env.tmpl | envsubst > private/${service}.env
   echo "Done."
 done
